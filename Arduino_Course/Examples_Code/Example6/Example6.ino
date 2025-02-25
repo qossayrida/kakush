cf51e9e04240811b9313d1a78c83a797b9b5d3e2
user <aqusay06@gmail.com>
2025-02-25 22:16:21 +0200

    Update Arduino slides

--- Arduino_Course/Examples_Code/Example6/Example6.ino
+++ Arduino_Course/Examples_Code/Example6/Example6.ino
@@ -1,29 +1,22 @@
-const int potPin = A0;       // Potentiometer connected to analog pin A0
-const int ledPin = 9;        // LED connected to digital pin 9 (PWM capable)
-const int button1Pin = 2;    // Button 1 connected to digital pin 2 (interrupt capable)
-const int button2Pin = 3;    // Button 2 connected to digital pin 3 (interrupt capable)
+const int potPin = A0;     
+const int ledPin = 9;        
+const int button1Pin = 2;    
 
 volatile bool ledState = false;
-volatile bool resetBrightness = false;
-int potValue = 0;            // Variable to store the potentiometer value
-int brightness = 0;          // Variable to store the LED brightness
+int potValue = 0;           
+int brightness = 0;          
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(button1Pin, INPUT);
-  pinMode(button2Pin, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(button1Pin), toggleLED, RISING);
-  attachInterrupt(digitalPinToInterrupt(button2Pin), resetLED, RISING);
 }
 
 void loop() {
-  if (resetBrightness) {
-    brightness = 128;  // Default brightness
-  } else {
-    potValue = analogRead(potPin);
-    brightness = map(potValue, 0, 1023, 0, 255);
-  }
+  
+  potValue = analogRead(potPin);
+  brightness = map(potValue, 0, 1023, 0, 255);
 
   if (ledState) {
     analogWrite(ledPin, brightness);
@@ -36,8 +29,4 @@ void loop() {
 
 void toggleLED() {
   ledState = !ledState;
-}
-
-void resetLED() {
-  resetBrightness = !resetBrightness;
 }
