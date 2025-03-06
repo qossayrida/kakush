862a35760f5dcf55c14787b8d6f7c81b15b77c83
user <aqusay06@gmail.com>
2025-03-06 11:26:15 +0200

    Add Slides for second Arduino course

--- Arduino_Course/Examples_Code/Example6/Example6.ino
+++ Arduino_Course/Examples_Code/Example6/Example6.ino
@@ -1,32 +1,31 @@
-const int potPin = A0;     
-const int ledPin = 9;        
-const int button1Pin = 2;    
+#define buttonPin 2 
 
-volatile bool ledState = false;
-int potValue = 0;           
-int brightness = 0;          
+int state = 1;
+const String valid_id = "1211553";
+volatile int trials = 2; 
+
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(button1Pin, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(button1Pin), toggleLED, RISING);
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  
-  potValue = analogRead(potPin);
-  brightness = map(potValue, 0, 1023, 0, 255);
+  if (Serial.available()) { 
+    String receivedData = Serial.readString();
+    receivedData.trim();
 
-  if (ledState) {
-    analogWrite(ledPin, brightness);
-  } else {
-    analogWrite(ledPin, 0);
+    if (receivedData == valid_id && trials > 0) { 
+      Serial.println("1");
+    } else {
+      trials--; 
+    }
   }
-
-  delay(10); // Small delay to avoid rapid changes
+  if (digitalRead(buttonPin)==1){
+      trials = 2;
+      Serial.print("Trials: ");
+      Serial.println(trials);
+  }
 }
 
-void toggleLED() {
-  ledState = !ledState;
-}
+
